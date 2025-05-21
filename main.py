1d28201f95d709dbd1e49fd7b9345e697cf1a735
Tehreem Ansari <tehreem@Mac.lan>
2025-05-21 10:40:11 -0700

    added icon without bg and updated main to take port from cmd too

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import argparse
 from app import app  # noqa: F401
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', type=int, default=5000, help='Port to run the Flask app on')
+    args = parser.parse_args()
+
+    app.run(host="0.0.0.0", port=args.port, debug=True)
+
